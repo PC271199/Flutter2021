eca3ee0ec2973ccb2e5729cfe07f532d04078787
BiRuVN <ngngquang99@gmail.com>
2021-04-12 22:23:45 +0700

    modify attribute name

--- models/user.dart
+++ models/user.dart
@@ -2,10 +2,10 @@ import 'package:mongo_dart/mongo_dart.dart';
 
 class User {
   final ObjectId user_id;
-  final String username;
+  final String email;
   final String password;
   final String role;
-  final String displayed_name;
+  final String fullName;
 
-  User(this.user_id, this.username, this.password, this.role, this.displayed_name);
+  User(this.user_id, this.email, this.password, this.role, this.fullName);
 }
